6da9dd58acabf35cd7ccc2e81dcbb4f6dd6759dd
Puneet <connect2puneet@gmail.com>
2022-09-21 08:45:38 +0000

    Changed the Instance Count

--- lambda/post.sh
+++ lambda/post.sh
@@ -1,9 +1,9 @@
 export IMG='number_5.png'
-export URL=`chalice url`
+export URL='https://by6if7vzs3.execute-api.eu-west-1.amazonaws.com/api/'
 
 # Download test image
 echo -e "\n********************** Downloading test image **********************\n"
-curl -o $IMG "https://mlpipeline-pipeline-data-488176068240.s3.eu-west-1.amazonaws.com/MNIST_6_0.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAXDKMPU2ILC7YWXNN%2F20220618%2Feu-west-1%2Fs3%2Faws4_request&X-Amz-Date=20220618T194117Z&X-Amz-Expires=604800&X-Amz-SignedHeaders=host&X-Amz-Signature=85f050e14971c45803da0c805ecfc45a6044c90c7343c0fe96e8a71cd44402a4"
+curl -o $IMG "https://mlpipeline-pipeline-data-488176068240.s3.eu-west-1.amazonaws.com/MNIST_6_0.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAXDKMPU2ILC7YWXNN%2F20220918%2Feu-west-1%2Fs3%2Faws4_request&X-Amz-Date=20220918T122421Z&X-Amz-Expires=604800&X-Amz-SignedHeaders=host&X-Amz-Signature=2474534a4a2fdc2f1b4264ca0ef83aedf349704f80027bb6fb4162ee42ce08df"
 
 echo -e "\n********************** POST request to Lambda **********************\n"
 (echo -n '{"data": "'; base64 $IMG; echo '"}') |
